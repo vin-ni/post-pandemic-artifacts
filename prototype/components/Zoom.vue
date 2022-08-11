dcec251a295242eb8ec93c8aa5938b3f659436b8
Vinzenz Aubry <vinzenzaubry@gmail.com>
2022-08-12 00:29:52 +0900

    added videos base

--- prototype/components/Zoom.vue
+++ prototype/components/Zoom.vue
@@ -2,23 +2,53 @@
 <template>
   <div class="zoom-component">
     <div class="video-wrapper">
-      <div id="vid-id-1"></div>
-      <div id="vid-id-2"></div>
-      <div id="vid-id-3"></div>
-      <div id="vid-id-4"></div>
-      <div id="vid-id-5"></div>
-      <div id="vid-id-6"></div>
-      <div id="vid-id-7"></div>
-      <div id="vid-id-8"></div>
-      <div id="vid-id-9"></div>
-      <div id="vid-id-10"></div>
-      <div id="vid-id-11"></div>
-      <div id="vid-id-12"></div>
+      <div ref="vidwrap-id-0" class="video-card">
+        <video ref="vid-id-0" src=""></video>
+      </div>
+      <div ref="vidwrap-id-1" class="video-card">
+        <video ref="vid-id-1" src=""></video>
+      </div>
+      <div ref="vidwrap-id-2" class="video-card">
+        <video ref="vid-id-2" src=""></video>
+      </div>
+      <div ref="vidwrap-id-3" class="video-card">
+        <video ref="vid-id-3" src=""></video>
+      </div>
+      <div ref="vidwrap-id-4" class="video-card">
+        <video ref="vid-id-4" src=""></video>
+      </div>
+      <div ref="vidwrap-id-5" class="video-card">
+        <video ref="vid-id-5" src=""></video>
+      </div>
+      <div ref="vidwrap-id-6" class="video-card">
+        <video ref="vid-id-6" src=""></video>
+      </div>
+      <div ref="vidwrap-id-7" class="video-card">
+        <video ref="vid-id-7" src=""></video>
+      </div>
+      <div ref="vidwrap-id-8" class="video-card">
+        <video ref="vid-id-8" src=""></video>
+      </div>
+      <div ref="vidwrap-id-9" class="video-card">
+        <video ref="vid-id-9" src=""></video>
+      </div>
+      <div ref="vid-wrapid-10" class="video-card">
+        <video ref="vid-id-10" src=""></video>
+      </div>
+      <div ref="vid-wrapid-11" class="video-card">
+        <video ref="vid-id-11" src=""></video>
+      </div>
+    </div>
+    <div class="bottom-bar">
+      <div class="left"><img src="/left.png" alt="" /></div>
+      <div class="middle"><img src="/icons.png" alt="" /></div>
+      <div class="right"><img src="/right.png" alt="" /></div>
     </div>
   </div>
 </template>
 
 <script>
+import playlist from '~/assets/js/playlist.json'
 export default {
   name: 'ZoomComponent',
   props: {},
@@ -26,8 +56,25 @@ export default {
     return {}
   },
   computed: {},
-  mounted() {},
+  mounted() {
+    this.setupVideos()
+  },
   destroyed() {},
-  methods: {},
+  methods: {
+    setupVideos() {
+      const keys = Object.keys(playlist.videos)
+      for (let i = 0; i < keys.length; i++) {
+        const element = playlist.videos[keys[i]]
+
+        // create video
+        const refString = `vid-id-${keys[i]}`
+        console.log(refString)
+        console.log(this.$refs[refString])
+        this.$refs[refString].src = `${playlist.root}${element.src}`
+
+        console.log(element)
+      }
+    },
+  },
 }
 </script>
